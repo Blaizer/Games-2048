c0486497383c691a9b7a4679cb08b2632443de30
Blaizer <blaiseroth@gmail.com>
2014-03-28 16:28:20 +1030

    Removed some boilerplate

--- lib/Games/2048.pm
+++ lib/Games/2048.pm
@@ -6,7 +6,25 @@ use warnings;
 
 =head1 NAME
 
-Games::2048 - The great new Games::2048!
+Games::2048 - Clone of a clone of the 2048 game
+
+=head1 SYNOPSIS
+
+ use Games::2048;
+ Games::2048->new->run;
+
+=head1 AUTHOR
+
+Blaise Roth <blaizer@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2014 by Blaise Roth.
+
+This is free software; you can redistribute and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+See L<http://dev.perl.org/licenses/> for more information.
 
 =cut
 
