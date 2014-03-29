319c70c3bffce288bd494a78a1b47b9b787a8e48
Blaizer <blaiseroth@gmail.com>
2014-03-29 16:34:22 +1030

    Separated Game into 2 classes A Board isa grid, and knows how to draw itself, but not how the game really works A Game isa board, and knows how the game works This change was mainly to move the ugly drawing code into another file... and it makes sense...

--- lib/Games/2048/Game.pm
+++ lib/Games/2048/Game.pm
@@ -2,33 +2,15 @@ package Games::2048::Game;
 use 5.01;
 use Moo;
 
-use Term::ANSIColor;
-use POSIX qw/floor ceil/;
-use List::Util qw/max min/;
-
-extends 'Games::2048::Grid';
+extends 'Games::2048::Board';
 
 has start_tiles  => is => 'ro', default => 2;
-has score        => is => 'rw', default => 0;
 has moved        => is => 'rw', default => 0;
-has needs_redraw => is => 'rw', default => 1;
 has quit         => is => 'rw', default => 0;
 has win          => is => 'rw', default => 0;
 has lose         => is => 'rw', default => 0;
 has keep_playing => is => 'rw', default => 0;
 has restart      => is => 'rw', default => 0;
-
-use constant {
-	# colors
-	BORDER_COLOR => "reverse",
-
-	# dimensions
-	BORDER_WIDTH => 2,
-	BORDER_HEIGHT => 1,
-
-	CELL_WIDTH => 7,
-	CELL_HEIGHT => 3,
-};
 
 sub each_vector {
 	(
@@ -83,19 +65,12 @@ sub run {
 		}
 
 		if ($self->needs_redraw) {
-			# restore cursor position to start of drawing
-			printf "\e[%dA", $self->total_height;
+			$self->restore_cursor;
 			$self->draw;
 		}
 
 		if ($self->lose or $self->win) {
-			my $message =
-				$self->win ? "You win!"
-				           : "Game over!";
-			my $offset = floor(($self->total_width - length($message)) / 2);
-
-			say " " x $offset, colored(uc $message, "bold"), "\n";
-
+			$self->draw_win($self->win);
 			last PLAY;
 		}
 
@@ -172,98 +147,6 @@ sub has_available_merges {
 			return 1 if $self->cells_can_merge($cell, $next);
 		}
 	}
-}
-
-sub draw {
-	my $self = shift;
-
-	$self->draw_border_horizontal;
-
-	for my $y (0..$self->size-1) {
-		for my $line (0..CELL_HEIGHT-1) {
-			$self->draw_border_vertical;
-
-			for my $x (0..$self->size-1) {
-				my $tile = $self->tile([$x, $y]);
-
-				if (defined $tile) {
-					my $value = $tile->value;
-					my $color = $self->tile_color($value);
-
-					my $lines = min(ceil(length($value) / CELL_WIDTH), CELL_HEIGHT);
-					my $first_line = floor((CELL_HEIGHT - $lines) / 2);
-					my $this_line = $line - $first_line;
-
-					if ($this_line >= 0 and $this_line < $lines) {
-						my $cols = min(ceil(length($value) / $lines), CELL_WIDTH);
-						my $string_offset = $this_line * $cols;
-						my $string_length = min($cols, length($value) - $string_offset, CELL_WIDTH);
-						my $cell_offset = floor((CELL_WIDTH - $string_length) / 2);
-
-						$self->draw_tile($cell_offset, $color);
-
-						my $string = substr($value, $string_offset, $string_length);
-						print colored($string, $color);
-
-						$self->draw_tile(CELL_WIDTH - $cell_offset - $string_length, $color);
-					}
-					else {
-						$self->draw_tile(CELL_WIDTH, $color);
-					}
-				}
-				else {
-					$self->draw_tile(CELL_WIDTH);
-				}
-			}
-
-			$self->draw_border_vertical;
-			say "";
-		}
-	}
-
-	$self->draw_border_horizontal;
-
-	$self->needs_redraw(0);
-}
-
-sub tile_color {
-	my ($self, $value) = @_;
-	!defined $value  ? ""
-	: $value <= 2    ? "reverse cyan"
-	: $value <= 4    ? "reverse green"
-	: $value <= 8    ? "reverse yellow"
-	: $value <= 16   ? "reverse blue"
-	: $value <= 32   ? "reverse magenta"
-	: $value <= 64   ? "reverse red"
-	: $value <= 2048 ? "reverse yellow"
-	                 : "reverse bright_yellow";
-}
-
-sub total_width {
-	my $self = shift;
-	$self->size * CELL_WIDTH + BORDER_WIDTH * 2;
-}
-
-sub total_height {
-	my $self = shift;
-	$self->size * CELL_HEIGHT + BORDER_HEIGHT * 2;
-}
-
-sub draw_border_horizontal {
-	my $self = shift;
-	say colored(" " x $self->total_width, BORDER_COLOR);
-}
-sub draw_border_vertical {
-	print colored("  ", BORDER_COLOR)
-}
-
-sub draw_tile {
-	my ($self, $width, $color) = @_;
-	return if $width < 1;
-	my $string = " " x $width;
-	print $color
-		? colored($string, $color)
-		: $string;
 }
 
 1;
