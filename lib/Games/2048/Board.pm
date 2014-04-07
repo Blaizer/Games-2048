5cc45fa7fd9d47a8624dd30d6b466dede20f0086
Blaise Roth <blaiseroth@gmail.com>
2014-04-07 13:57:09 +0930

    Colors fixed for Win32

--- lib/Games/2048/Board.pm
+++ lib/Games/2048/Board.pm
@@ -195,7 +195,8 @@ sub tile_color {
 		: $value < 4096    ? ansifg("F9F6F2") . ansibg("EDC22E") . color("bold")
 		                   : ansifg("F9F6F2") . ansibg("3C3A32") . color("bold");
 	}
-	my $bright = $^O eq "MSWin32" ? "bold " : "bright_";
+	my $bright = $^O eq "MSWin32" ? "underline " : "bright_";
+	my $bold = $^O eq "MSWin32" ? "underline" : "bold";
 	return color (
 		!defined $value    ? "reset"
 		: $value < 4       ? "reverse cyan"
@@ -205,7 +206,7 @@ sub tile_color {
 		: $value < 64      ? "reverse magenta"
 		: $value < 128     ? "reverse red"
 		: $value < 4096    ? "reverse yellow"
-		                   : "reverse bold"
+		                   : "reverse $bold"
 	);
 }
 
